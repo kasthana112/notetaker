07df1d0f7cf051c6bf861ac04c41ba9eec8e1928
kasthana112 <kanika.asthana@gmail.com>
2023-03-02 20:03:02 -0600

    fixed errors

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,49 +1,14 @@
-const router = require('express').Router();
-const fs = require('fs');
-const uuidv1 = require('uuid/v1');
+const app = require('express').Router();
+const path = require('path');
 
 
-
-router.get('/notes', (req, res) =>{
-    console.log("get notes route");
-    
-fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    res.json(JSON.parse(data));
-    
-}
-)
-})
-    router.post('/notes', (req, res) => {
-        console.log("post notes route");
-        let newNoteToSave = {
-            ...req.body, id:uuidv1()}
-    //    // notes.push(newNote);
-    //     updateDb();
-
-    //     db.push(newNoteToSave);
-    //     fs.writeFileSync('./db/db.json', JSON.stringify(db),function(err){
-    //         if(err) throw err;
-    //     // });//
+    app.get('/', (req, res) =>{
+        res.sendFile(path.join(__dirname, '../public/index.html'))
     });
-
-    function updateDb() {
-        fs.writeFile('./db/db.json', JSON.parse(notes), (err) => {
-            if (err) throw err;
-        });
-
-        //res.json(notes);/
-        console.log(notes);
-    }
-//comments//
-
-
-router.delete('/notes/:id', (req, res) => {
-    let id = req.params.id;
-    notes.splice(id, 1);
-    updateDb();
-})
+   app.get('/notes', (req, res) =>{
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
 
 
 
-module.exports = router;
+module.exports = app;
--- server.js
+++ server.js
@@ -1,33 +1,16 @@
 const express = require('express');
-const path = require('path');
-const api = require('./routes/apiRoutes.js');
-
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const fs = require('fs');
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-const app = express();
-
-// Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
-
+app.use(express.json());
 app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-// GET Route for homepage
-// localhost:3001/
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-// GET Route for feedback page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-app.post('/notes', (req, res) => {
-    // save the new note using req.body
-})
-
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`)
+});
