51a627e152a8b1a8d39469cfd18b330fc2165cb3
kasthana112 <kanika.asthana@gmail.com>
2023-04-02 16:38:02 -0500

    fixed codes, working now

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs= require('fs');
 const path = require('path');
-const uuid = require('file path')
+const {v4: uuidv4} = require('uuid')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -9,26 +9,43 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// http://localhost:3001/
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+// http://localhost:3001/notes
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-app.get('/api/notes', (req, res) => {
+// http://localhost:3001/api/notes
+app.post('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) throw err;
         const notes = JSON.parse(data);
+        console.log(notes);
         const newNotes = req.body;
-        newNotes.id = uuid();
+        newNotes.id = uuidv4();
+        console.log(newNotes);
         notes.push(newNotes);
         fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
             if (err) throw err;
             res.json(notes);
         });
+
     });
+
+});
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) =>
+    {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        res.json(notes);
+    });
+
 });
 
 
